89eb0e6918799e23e71b28c6776d4026c7342bdb
Dinesh-Adhithya-H <68704516+Dinesh-Adhithya-H@users.noreply.github.com>
2021-07-24 01:23:37 +0530

    Update IslandIterator.jl

--- src/IslandIterator.jl
+++ src/IslandIterator.jl
@@ -18,7 +18,7 @@ function Base.iterate(iter::IslandIterator, state = nothing)
     pos == length(iter.read_start_pos) && return ((1,iter.read_length), IslandIteratorState(pos+1))
     pos>length(iter.read_start_pos) && return nothing
 
-    while iter.read_start_pos[pos]+iter.read_length-1>=iter.read_start_pos[pos+1]
+    while iter.read_start_pos[pos]+iter.read_length>=iter.read_start_pos[pos+1]
         pos += 1
         no_reads+=1
         pos == length(iter.read_start_pos) && return  ((no_reads,iter.read_length+iter.read_start_pos[pos]-iter.read_start_pos[pos-no_reads+1]), IslandIteratorState(pos + 1))
