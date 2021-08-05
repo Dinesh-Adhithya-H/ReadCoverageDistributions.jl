084e3b34eda7d380f83ac0c1e2fcfca092ede422
Peter F Arndt <pfarndt@users.noreply.github.com>
2021-08-05 12:26:28 -0400

    some corretions and making code nicer

--- src/ReadCoverageDistributions.jl
+++ src/ReadCoverageDistributions.jl
@@ -3,8 +3,22 @@ module ReadCoverageDistributions
 include("IslandIterator.jl")
 include("HistogramReadCoverages.jl")
 include("OceanIslandsDistributions.jl")
+include("merge_sort.jl")
 
-export simulate_and_get_coverage_hist_2,simulate_oceansislands,no_islands_vs_no_reads_simulation,no_oceans_vs_no_reads_simulation,avg_length_oceans_vs_no_reads_simulation,avg_length_islands_vs_no_reads_simulation,simulate_and_get_coverage_hist_3,no_reads_vs_no_islands,simulate_readcoverage,Para,increment!,hist_add!,vec_hist_add!,simulate_and_get_coverage_hist,IslandIteratorState,IslandIterator,iterate,merge_sorted_arrays
+export simulate_and_get_coverage_hist_2,
+	simulate_oceansislands,
+	no_islands_vs_no_reads_simulation,
+	no_oceans_vs_no_reads_simulation,
+	avg_length_oceans_vs_no_reads_simulation,
+	avg_length_islands_vs_no_reads_simulation,
+	simulate_and_get_coverage_hist_3,
+	no_reads_vs_no_islands,simulate_readcoverage,
+	Para,
+	increment!,
+	hist_add!,vec_hist_add!,
+	simulate_and_get_coverage_hist,
+	IslandIterator,
+	merge_sorted_arrays
 
 
 end
